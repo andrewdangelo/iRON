2bab183185e8cb9e27513c98bf34065f3f43313e
Bryan Kyritz <bryan@securemeeting.org>
2021-04-19 17:39:48 -0400

    init

---
+++ src/main/udpserver.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net" //the net Package net provides a portable interface for network I/O, including TCP/IP, UDP, domain name resolution, and Unix domain sockets.
+)
+
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+	_, err := conn.WriteToUDP([]byte("From server: Hello I got your message "), addr)
+	if err != nil {
+		fmt.Printf("Couldn't send response %v", err)
+	}
+}
+
+func main() {
+	p := make([]byte, 2048)
+	addr := net.UDPAddr{
+		Port: 1234,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+
+	fmt.Println("Started udpserver at ", addr.IP, ":", addr.Port)
+
+	ser, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return
+	}
+	for {
+		_, remoteaddr, err := ser.ReadFromUDP(p)
+		fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+		if err != nil {
+			fmt.Printf("Some error  %v", err)
+			continue
+		}
+		go sendResponse(ser, remoteaddr)
+	}
+}
